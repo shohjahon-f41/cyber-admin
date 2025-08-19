cde1b86cf8aa9b74ad60c809502f5b1a87e05191
shohjahon-f41 <aizensolo086@gmail.com>
2025-08-19 18:20:48 +0500

    Auth 2

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -4,7 +4,24 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext(null);
 
 function AuthProvider({ children }) {
-    const navigate = useNavigate()
-    let locale = localStorage.getItem("token") || ""
-    const [auth, setAuth] = useState(!!locale)
+  const navigate = useNavigate();
+  let locale = localStorage.getItem("token") || "";
+  const [auth, setAuth] = useState(!!locale);
+
+  function setUser() {
+    navigate("/");
+    setAuth(true);
+  }
+
+  function Logout() {
+    navigate("/login");
+    localStorage.clear();
+    setAuth(false);
+  }
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, setUser, Logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
